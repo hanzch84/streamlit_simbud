e0094c9c7643e3d97af8c3d06761a08dc077613e
hanzch84 <73715848+hanzch84@users.noreply.github.com>
2026-01-12 14:15:24 +0900

    Update version in the budget simulator UI

--- fitbudget.py
+++ fitbudget.py
@@ -365,7 +365,7 @@ def calculate_budget(budget, labels, prices, base_quantity, limited_quantity):
 result_list, result_prices = [], []
 
 st.title("편리한 예산🍞만들기")
-st.markdown('<p style="color: #a8a888;text-align: right;">SimBud beta (Budget Simulator V1.00)by 교사 박현수, 버그 및 개선 문의: <a href="mailto:hanzch84@gmail.com">hanzch84@gmail.com</a></p>', unsafe_allow_html=True)
+st.markdown('<p style="color: #a8a888;text-align: right;">SimBud beta (Budget Simulator V1.50)by 교사 박현수, 버그 및 개선 문의: <a href="mailto:hanzch84@gmail.com">hanzch84@gmail.com</a></p>', unsafe_allow_html=True)
 
 col_label_budget, col_input_budget = st.columns([3, 7])
 with col_label_budget:
